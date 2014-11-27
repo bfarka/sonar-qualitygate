9ddc83fed1d9f090504780662f4050eb14de0f6d
berndfarka <bernd.farka@gmail.com>
2014-11-27 21:30:55 +0100

    fix broken test

--- src/test/groovy/at/bfarka/sonar/qualitygate/QualityGateStateTest.groovy
+++ src/test/groovy/at/bfarka/sonar/qualitygate/QualityGateStateTest.groovy
@@ -2,6 +2,7 @@ package at.bfarka.sonar.qualitygate
 
 import org.junit.Test
 
+import static at.bfarka.sonar.qualitygate.QualityGateState.NO_RESULT
 import static at.bfarka.sonar.qualitygate.QualityGateState.OK
 import static at.bfarka.sonar.qualitygate.QualityGateState.fromString
 import static org.hamcrest.Matchers.equalTo
@@ -18,6 +19,12 @@ class QualityGateStateTest {
         assertThat(QualityGateState.WARNING, equalTo(fromString("WARN")))
         assertThat(QualityGateState.ERROR, equalTo(fromString("ERROR")))
 
+    }
+
+    @Test
+    public void testNonExistingMapp(){
+        assertThat(NO_RESULT, equalTo(fromString("NOTOK")))
+        assertThat(NO_RESULT, equalTo(fromString(null)))
     }
 
 }
